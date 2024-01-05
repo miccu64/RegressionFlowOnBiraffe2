56e12fcda772ce45ae07cf5f18f8f4740eda9700
miccu64 <98konrad98@gmail.com>
2024-01-05 11:35:17 +0100

    Working blob generation

--- biraffe2_helpers/biraffe2_utils.py
+++ biraffe2_helpers/biraffe2_utils.py
@@ -2,22 +2,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-# TODO: delete decode_obj() and draw_sdd_heatmap() if not necessary
-def decode_obj(x_valence: float, y_arousal: float):
-    obj_size = 0.001
-    class_id = 1
-    object = np.array([x_valence, y_arousal, x_valence + obj_size, y_arousal + obj_size, class_id])
-    object = np.expand_dims(np.expand_dims(np.expand_dims(object, 0), 0), 3).astype(np.float32)
-
-    # tl = top-left corner of object, tr = top-right, bl = bottom-left
-    x_tl = object[:, :, 0:1, :]
-    y_tl = object[:, :, 1:2, :]
-    x_br = object[:, :, 2:3, :]
-    y_br = object[:, :, 3:4, :]
-    object = np.concatenate((x_tl, y_tl, x_br, y_br, object[:, :, 4:5, :]), axis=2)
-    return object
-
-def draw_biraffe2_heatmap(img: np.ndarray, log_px_pred, X, Y, save_path):
+def draw_biraffe2_heatmap(
+    x_valence: [float], y_arousal: [float], log_px_pred, log_py_pred, X, Y, save_path: str
+):
     def transparent_cmap(cmap, N=255):
         "Copy colormap and set alpha values"
         mycmap = cmap
@@ -25,19 +12,28 @@ def draw_biraffe2_heatmap(img: np.ndarray, log_px_pred, X, Y, save_path):
         mycmap._lut[:, -1] = np.clip(np.linspace(0, 1.0, N + 4), 0, 1.0)
         return mycmap
 
-    Z = log_px_pred.reshape(-1)
-    Z = np.exp(Z)
-    vmax = np.max(Z)
-    vmin = np.min(Z)
-    h, w, _ = img.shape
-    plt.figure(
-        figsize=(
-            w // 25,
-            h // 25,
-        )
-    )
-    plt.imshow(img)
-    plt.contourf(X, Y, Z.reshape(X.shape), vmin=vmin, vmax=vmax, cmap=transparent_cmap(plt.cm.jet), levels=20)
+    Z_diff = np.exp(log_py_pred.reshape(-1))
+    vmax_diff = np.max(Z_diff)
+    vmin_diff = np.min(Z_diff)
 
-    plt.axis("off")
-    plt.savefig(save_path, format="png", bbox_inches="tight", pad_inches=0)
+    figure, ax = plt.subplots()
+    values_range = [-1, 1]
+    plt.xlim(values_range)
+    plt.ylim(values_range)
+    plt.scatter(x_valence, y_arousal)
+
+    cmap_diff = transparent_cmap(plt.cm.viridis)
+    ax.contourf(
+        X,
+        Y,
+        Z_diff.reshape(X.shape),
+        vmin=vmin_diff,
+        vmax=vmax_diff,
+        cmap=cmap_diff,
+        levels=20
+    )
+
+    figure.savefig(save_path, format="png", bbox_inches="tight")
+    plt.close()
+
+
--- test_biraffe2.py
+++ test_biraffe2.py
@@ -1,8 +1,5 @@
-import io
 import json
 import os
-import cv2
-from matplotlib import pyplot as plt
 import numpy as np
 import torch
 from biraffe2_helpers.biraffe2_utils import draw_biraffe2_heatmap
@@ -11,19 +8,15 @@ from data_regression_biraffe2_test import Biraffe2DatasetTest
 import mmfp_utils
 from args import get_args
 from models.networks_regression_biraffe2 import HyperRegression
-from utils import draw_hyps, draw_sdd_heatmap
 
 
-def get_grid_logprob(
-        height, width, x, model
-):
-    x_sp = np.linspace(0, width - 1, width // 1)
-    y = np.linspace(0, height - 1, height // 1)
+def get_grid_logprob(x, model):
+    x_sp = np.linspace(-1, 1, 100)
+    y = np.linspace(-1, 1, 100)
     X, Y = np.meshgrid(x_sp, y)
     XX = np.array([X.ravel(), Y.ravel()]).T
     _, _, (log_py_grid, log_px_grid) = model.get_logprob(
-        x,
-        torch.tensor(XX).unsqueeze(0).to(args.gpu)
+        x, torch.tensor(XX).unsqueeze(0).to(args.gpu)
     )
     return (X, Y), (log_px_grid.detach().cpu().numpy(), log_py_grid.detach().cpu().numpy())
 
@@ -31,9 +24,9 @@ def get_grid_logprob(
 def main(args):
     args.gpu = 0
     args.num_blocks = 1
-    args.resume_checkpoint = 'checkpoints/biraffe2_v2/checkpoint-latest.pt'
+    args.resume_checkpoint = "checkpoints/biraffe2_v2/checkpoint-latest.pt"
     args.data_dir = "data/BIRAFFE2"
-    args.dims = '32-32-32'
+    args.dims = "32-32-32"
 
     test_data = Biraffe2DatasetTest(args.data_dir)
     test_loader = torch.utils.data.DataLoader(
@@ -49,10 +42,10 @@ def main(args):
     resume_checkpoint = args.resume_checkpoint
     print(f"Resume Path: {resume_checkpoint}")
     checkpoint = torch.load(resume_checkpoint, map_location="cpu")
-    model_serialize = checkpoint['model']
+    model_serialize = checkpoint["model"]
     model.load_state_dict(model_serialize)
     model.eval()
-    save_path = os.path.join(os.path.split(resume_checkpoint)[0], 'results')
+    save_path = os.path.join(os.path.split(resume_checkpoint)[0], "results")
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     nll_px_sum = 0
@@ -79,8 +72,7 @@ def main(args):
             log_py = log_py.cpu().detach().numpy().squeeze()
             log_px = log_px.cpu().detach().numpy().squeeze()
 
-            hyps_name = f"{subject}-{row}-hyps.jpg"
-            print(hyps_name)
+            print(f"Subject: {subject}, row: {row}")
             print("nll_x", str(-1.0 * log_px))
             print("nll_y", str(-1.0 * log_py))
             print("nll_(x+y)", str(-1.0 * log_px + log_py))
@@ -90,33 +82,23 @@ def main(args):
             counter = counter + 1.0
             y_pred = y_pred.cpu().detach().numpy().squeeze()
 
-            valence = y_pred[:, 0]
-            arousal = y_pred[:, 1]
-
-            values_range = [-1, 1]
-            plt.xlim(values_range)
-            plt.ylim(values_range)
-            plt.scatter(valence, arousal)
-
-            buf = io.BytesIO()
-            plt.savefig(buf, format='png')
-            buf.seek(0)
-            img_matlike = cv2.imdecode(np.frombuffer(buf.read(), np.uint8), 1)
-            plt.close()
-
-            cv2.imwrite(os.path.join(save_path, hyps_name), img_matlike)
-
             multimod_emd = mmfp_utils.wemd_from_pred_samples(y_pred)
             multimod_emd_sum += multimod_emd
             print("multimod_emd", multimod_emd)
 
-            (X, Y), (log_px_grid, log_py_grid) = get_grid_logprob(img_matlike.shape[0], img_matlike.shape[1], x, model)
+            valence = y_pred[:, 0]
+            arousal = y_pred[:, 1]
+
+            (X, Y), (log_px_grid, log_py_grid) = get_grid_logprob(x, model)
 
             draw_biraffe2_heatmap(
-                img=img_matlike,
+                x_valence=valence,
+                y_arousal=arousal,
                 log_px_pred=log_px_grid,
-                X=X, Y=Y,
-                save_path=os.path.join(save_path, f"{subject}-{row}-heatmap.png")
+                log_py_pred=log_py_grid,
+                X=X,
+                Y=Y,
+                save_path=os.path.join(save_path, f"{subject}-{row}-heatmap.png"),
             )
 
             result_row = {
@@ -124,7 +106,7 @@ def main(args):
                 "row": row,
                 "nll_x": float(-1.0 * log_px),
                 "nll_y": float(-1.0 * log_py),
-                "multimod_emd": float(multimod_emd)
+                "multimod_emd": float(multimod_emd),
             }
             results.append(result_row)
 
@@ -135,6 +117,7 @@ def main(args):
     with open(os.path.join(save_path, "metrics.json"), "w") as f:
         json.dump(results, f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = get_args()
     main(args)
